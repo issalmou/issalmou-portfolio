96aae12f01f13364bfb98ce0fb0f5f703a6d6f4b
Issalmou Adaaiche <154550142+issalmou@users.noreply.github.com>
2025-10-05 23:33:25 +0100

    add fade to sections of page notfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -31,12 +31,12 @@ export default function NotFound({ language }) {
             justifyContent: "center",
             alignItems: "center",
             flexDirection: "column",
-        }}>
-            <div>
+        }} data-aos="fade-up">
+            <div data-aos="fade-down" data-aos-delay="100">
                 <h1 className="error-title link-underline">{texts.notFound.title}</h1>
                 <p className="zoom-area">{texts.notFound.description} <br />
                     {texts.notFound.homeButton} </p>
-                <section className="error-container">
+                <section className="error-container" data-aos="fade-up" data-aos-delay="150">
                     <span className="four"><span className="screen-reader-text">4</span></span>
                     <span className="zero"><span className="screen-reader-text">0</span></span>
                     <span className="four"><span className="screen-reader-text">4</span></span>
